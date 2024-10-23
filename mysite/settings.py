0826b4a7a0c11a6a04c683463c26c34eb8bf6f42
Navro <navro@navros-mbp.mynetworksettings.com>
2024-10-22 20:25:58 -0400

    ff

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+# Add this line to specify the directory for collected static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -136,3 +139,4 @@ def _patch_sqlite():
 
 
 _patch_sqlite()
+
